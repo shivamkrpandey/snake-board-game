397227fc86244756a52dba0fc95f4d19d04eed44
Shivam Pandey <shivam.pandey@causecode.com>
2019-02-03 21:58:06 +0530

    docs changes

--- src/main/java/com/boardgame/game/Box.java
+++ src/main/java/com/boardgame/game/Box.java
@@ -1,7 +1,7 @@
 package com.boardgame.game;
 
 /**
- * It represents boxes in the board game of {@link BoardBasedGame} game. Here it represents the
+ * It represents boxes in the board game of {@link BoardBasedGame} game.
  */
 public class Box {
     private int position;
